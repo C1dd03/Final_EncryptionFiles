8c33aa91fe6fa4ecc823f1b1c37f61e6cffda508
jerwil445 <jerwil445astig@gmail.com>
2025-11-11 05:22:31 +0800

    add restrition js code and disable back browser button

--- views/auth/auth.php
+++ views/auth/auth.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Pick&Match | Ecommerce Website Design</title>
-  <link rel="stylesheet" href="/../encryption/public/css/style.css" />
+  <link rel="stylesheet" href="/encryption/public/css/style.css" />
+  <link rel="stylesheet" href="/encryption/public/css/style.php">
   <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" /> -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
