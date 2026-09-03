45129fc2ec8e3b88a35d9ca217919721eb711e72
jerwil445 <jerwil445astig@gmail.com>
2026-09-03 22:42:24 +0800

    change and add

---
+++ config/smtp.local.php
@@ -0,0 +1,16 @@
+<?php
+
+// Local XAMPP-only secrets. This file is ignored by Git.
+return [
+    'SMTP_HOST' => 'smtp.gmail.com',
+    'SMTP_PORT' => '587',
+    'SMTP_USERNAME' => 'jerwil445astig@gmail.com',
+    'SMTP_PASSWORD' => 'vqns fvuj eliq worb',
+    'SMTP_ENCRYPTION' => 'tls',
+    'SMTP_FROM_EMAIL' => 'jerwil445astig@gmail.com',
+    'SMTP_FROM_NAME' => 'ArgiConnect',
+    'SMTP_TIMEOUT' => '15',
+    'APP_ENV' => 'production',
+    'OTP_DEV_LOG' => 'false',
+    'OTP_DEV_SHOW' => 'false',
+];
